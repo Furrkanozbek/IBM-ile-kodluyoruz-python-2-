3deb7a9a059bb72fe5a3ba87a2bc13c8bd420407
Muhammet Furkan Özbek <119418590+Furrkanozbek@users.noreply.github.com>
2024-07-22 04:29:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import math
-
+#noktaları points listi altında belirledik
 points = [(1, 2), (4, 5), (3, 1), (7, 6)]
+#öklid mesafe hesaplama fonksiyonu burada sqrt fonksiyonu karekök için kullanılır.
 def euclideanDistance(point1, point2):
     return math.sqrt((point2[0] - point1[0])**2 + (point2[1] - point1[1])**2)
+#mesafeler hesaplanıp distances adlı listeye ekleniyor
 distances = []
 for i in range(len(points)):
     for j in range(i + 1, len(points)):
         distance = euclideanDistance(points[i], points[j])
         distances.append(distance)
+#min fonksiyonu kullanarak distances listesindeki en küçük eleman bulunur.
 min_distance = min(distances)
 print("Minimum Öklid mesafesi:", min_distance)
